fb26ce1ae4c6f9314095d95a9b2804f836232a7b
Oscar Shoudt <shoudto@gmail.com>
2019-08-05 14:36:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -12,14 +12,17 @@ class String
 
   def question?
     if self.end_with?("?")
-      true 
+      true
     else
       false
     end
   end
 
   def exclamation?
+    if self.end_with?("")
 
+
+    end
   end
 
   def count_sentences
